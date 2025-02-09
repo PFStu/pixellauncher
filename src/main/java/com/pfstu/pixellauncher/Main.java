162a914c6339977e7ad4d2b9ca912d913c38f881
PFStu <ch_bow@126.com>
2025-02-09 19:01:05 +0800

    终于可以切换窗口了

--- src/main/java/com/pfstu/pixellauncher/Main.java
+++ src/main/java/com/pfstu/pixellauncher/Main.java
@@ -1,8 +1,8 @@
 package com.pfstu.pixellauncher;
 
+import com.pfstu.pixellauncher.Modules.Menu.MainController;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Orientation;
 import javafx.scene.Parent;
@@ -11,53 +11,65 @@ import javafx.scene.control.SplitPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Objects;
 
 public class Main extends Application {
-    SplitPane splitPane = new SplitPane();
-    Parent root1;
+    private SplitPane splitPane = new SplitPane();
+    private Parent root1;
 
-    public Main() throws IOException {
+    // 添加静态实例以便控制器访问
+    private static Main instance;
+
+    public Main() {
+        instance = this;
+    }
+
+    public static Main getInstance() {
+        return instance;
     }
 
     @Override
     public void start(Stage stage) throws IOException {
-        // 菜单栏
-        FXMLLoader loader1 = new FXMLLoader(getClass().getResource("Modules/Menu/MainView.fxml"));
+        // 加载菜单栏
+        FXMLLoader loader1 = new FXMLLoader(
+                getClass().getResource("/com/pfstu/pixellauncher/Modules/Menu/MainView.fxml")
+        );
         root1 = loader1.load();
 
-        // 下载
-        FXMLLoader loader2 = new FXMLLoader(getClass().getResource("Modules/Download/MainView.fxml"));
+        // 获取菜单控制器并传递 Main 实例
+        MainController menuController = loader1.getController();
+        menuController.setMainApp(this);
+
+        // 加载默认右侧界面（例如下载界面）
+        FXMLLoader loader2 = new FXMLLoader(
+                getClass().getResource("/com/pfstu/pixellauncher/Modules/Download/MainView.fxml")
+        );
         Parent root2 = loader2.load();
-        // 启动
-        FXMLLoader loader3 = new FXMLLoader(getClass().getResource("Modules/Launch/MainView.fxml"));
-        Parent root3 = loader3.load();
 
         splitPane.getItems().addAll(root1, root2);
         splitPane.setOrientation(Orientation.HORIZONTAL);
-        splitPane.setStyle("-fx-background-color: transparent;");
-        splitPane.lookupAll(".split-pane-divider").forEach(divider -> {
-            divider.setMouseTransparent(true);
-        });
         splitPane.setDividerPositions(0.1);
-        Scene mainScene = new Scene(splitPane);
+
+        Scene scene = new Scene(splitPane, 800, 600);
         stage.setTitle("Pixel Launcher Dev");
-        stage.setScene(mainScene);
+        stage.setScene(scene);
         stage.show();
     }
 
-    @FXML
-    public void switchWindow(String Window, String fxml) throws IOException {
+    public void switchWindow(String window, String fxmlPath) {
         Platform.runLater(() -> {
-            Parent rootSelected = null;
             try {
-                rootSelected = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Modules/" + Window + "/" + fxml)));
+                // 使用绝对路径加载 FXML
+                FXMLLoader loader = new FXMLLoader(
+                        getClass().getResource("/com/pfstu/pixellauncher/Modules/" + window + "/" + fxmlPath)
+                );
+                Parent newRoot = loader.load();
+
+                splitPane.getItems().clear();
+                splitPane.getItems().addAll(root1, newRoot);
+                splitPane.setDividerPositions(0.1);
             } catch (IOException e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
             }
-            splitPane.getItems().clear(); // 清空现有内容
-            splitPane.getItems().addAll(root1, rootSelected);
-            splitPane.setDividerPositions(0.1);
         });
     }
 
--- src/main/java/com/pfstu/pixellauncher/Modules/Menu/MainController.java
+++ src/main/java/com/pfstu/pixellauncher/Modules/Menu/MainController.java
@@ -4,16 +4,21 @@ import com.pfstu.pixellauncher.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-import java.io.IOException;
-
 public class MainController {
-    @FXML
-    public void switchToDownloadWindow(ActionEvent actionEvent) throws IOException {
-        new com.pfstu.pixellauncher.Main().switchWindow("Download", "MainView.fxml");
+    private Main mainApp;
+
+    // 设置 Main 实例的引用
+    public void setMainApp(Main mainApp) {
+        this.mainApp = mainApp;
     }
 
     @FXML
-    public void switchToLaunchWindow(ActionEvent actionEvent) throws IOException {
-        new com.pfstu.pixellauncher.Main().switchWindow("Launch", "MainView.fxml");
+    public void switchToDownloadWindow(ActionEvent actionEvent) {
+        mainApp.switchWindow("Download", "MainView.fxml");
+    }
+
+    @FXML
+    public void switchToLaunchWindow(ActionEvent actionEvent) {
+        mainApp.switchWindow("Launch", "MainView.fxml");
     }
 }
